685376e77df95fe586d48333cd1f16311b8c7647
Mike Dilger <mike@mikedilger.com>
2025-06-25 22:15:20 +1200

    impl ClientCertVerifier for SelfSignedCertificateVerifier

--- src/cert.rs
+++ src/cert.rs
@@ -3,10 +3,11 @@ use alloc::sync::Arc;
 use alloc::vec::Vec;
 use core::ops::Deref;
 
-use rustls::client::danger;
+use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
 use rustls::crypto::WebPkiSupportedAlgorithms;
 use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
-use rustls::{DigitallySignedStruct, SignatureScheme};
+use rustls::server::danger::{ClientCertVerified, ClientCertVerifier};
+use rustls::{DigitallySignedStruct, DistinguishedName, SignatureScheme};
 
 /// This is a certificate verifier that allows all self-signed certificates.
 /// It does NOT require any particular known certificate; rather, any self-signed certificate
@@ -38,7 +39,7 @@ impl SelfSignedCertificateVerifier {
     }
 }
 
-impl danger::ServerCertVerifier for SelfSignedCertificateVerifier {
+impl ServerCertVerifier for SelfSignedCertificateVerifier {
     fn verify_server_cert(
         &self,
         end_entity: &CertificateDer<'_>,
@@ -46,7 +47,7 @@ impl danger::ServerCertVerifier for SelfSignedCertificateVerifier {
         _server_name: &ServerName<'_>,
         _ocsp_response: &[u8],
         _now: UnixTime,
-    ) -> Result<danger::ServerCertVerified, rustls::Error> {
+    ) -> Result<ServerCertVerified, rustls::Error> {
         if let Some(expected_key_bytes) = &self.expected_key_bytes {
             let (_, cert) = x509_parser::parse_x509_certificate(end_entity.deref())
                 .map_err(|e| rustls::Error::Other(rustls::OtherError(Arc::new(e))))?;
@@ -61,7 +62,7 @@ impl danger::ServerCertVerifier for SelfSignedCertificateVerifier {
             }
         }
 
-        Ok(danger::ServerCertVerified::assertion())
+        Ok(ServerCertVerified::assertion())
     }
 
     fn verify_tls12_signature(
@@ -69,7 +70,7 @@ impl danger::ServerCertVerifier for SelfSignedCertificateVerifier {
         message: &[u8],
         cert: &CertificateDer<'_>,
         dss: &DigitallySignedStruct,
-    ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
         rustls::crypto::verify_tls12_signature(message, cert, dss, &self.algorithms)
     }
 
@@ -78,11 +79,62 @@ impl danger::ServerCertVerifier for SelfSignedCertificateVerifier {
         message: &[u8],
         cert: &CertificateDer<'_>,
         dss: &DigitallySignedStruct,
-    ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
         rustls::crypto::verify_tls13_signature(message, cert, dss, &self.algorithms)
     }
 
     fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
         self.schemes.clone()
+    }
+}
+
+impl ClientCertVerifier for SelfSignedCertificateVerifier {
+    fn root_hint_subjects(&self) -> &[DistinguishedName] {
+        &[]
+    }
+
+    fn verify_client_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _now: UnixTime,
+    ) -> Result<ClientCertVerified, rustls::Error> {
+        // We do not require certificate verification
+        Ok(ClientCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        rustls::crypto::verify_tls12_signature(message, cert, dss, &self.algorithms)
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        rustls::crypto::verify_tls13_signature(message, cert, dss, &self.algorithms)
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        self.schemes.clone()
+    }
+
+    fn offer_client_auth(&self) -> bool {
+        true
+    }
+
+    fn client_auth_mandatory(&self) -> bool {
+        false
+    }
+
+    fn requires_raw_public_keys(&self) -> bool {
+        // Self-signed are acceptable
+        false
     }
 }
